49582a3b415b6ee1ec1f14ba543f2def149e1de4
mikicola <michela.colangelo@gmail.com>
2022-04-26 17:17:38 +0200

    passo dato name

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,6 @@
     <title>HOME</title>
 </head>
 <body>
-    <h1>Hello World</h1>
+    <h1>Hello {{ $name }}</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-})->name('home');
+    $data = [
+        'name' => 'Mr. Mondo',
+    ];
+    return view('home', $data);
+});
 
